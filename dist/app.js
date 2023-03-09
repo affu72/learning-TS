d1c607dd1ac5ec03c5f1090b5b2e1628faef250a
Afzal Nomani <afzalnomani1@gmail.com>
2023-03-09 14:59:07 +0530

    OOP in Typescript

--- dist/app.js
+++ dist/app.js
@@ -33,7 +33,59 @@ function foo(a, b) {
 console.log(foo(10, 5));
 console.log(foo(5));
 //**this
-const arrowFun = () => console.log("'this' will be the surrounding of 'this', but as I am working in strinct environment so this will be undefined " +
-    this);
-arrowFun();
+// const arrowFun = () =>
+//   console.log(
+//     "'this' will be the surrounding of 'this', but as I am working in strinct environment so this will be undefined " +
+//       this
+//   );
+// arrowFun();
 console.log(!!"a");
+/***************************** CLASSES ***********************/
+class Person {
+    constructor(id, firstName, lastName) {
+        // this.firstName = firstName;
+        // this.lastName = lastName;
+        // this.friends = [];
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        // firstName: string;
+        // lastName: string;
+        this.friends = [];
+        console.log("calling from object");
+    }
+    describe() {
+        console.log(`Person-${this.id}: ${this.firstName}  ${this.lastName}`);
+    }
+    addFriends(friend) {
+        this.friends.push(friend);
+    }
+    printFriendList() {
+        console.log(this.friends);
+    }
+}
+const person1 = new Person(1, "Afzal", "Nomani");
+person1.describe();
+person1.addFriends("Aditya");
+person1.addFriends("Imran");
+person1.printFriendList();
+//Inheritance
+class Student extends Person {
+    constructor(id, firstname, lastName, cources) {
+        super(id, firstname, lastName);
+        this.cources = cources;
+        this.cources = cources;
+    }
+}
+const stu1 = new Student(2, "karan", "Yadav", ["Node", "React"]);
+console.log(stu1);
+// console.log(person1.friends);
+// const person1Copy = { describe: person1.describe };
+// console.log(person1Copy.describe()); // for this 'this' will be undefined
+//quick fix
+// const person1Copy = {
+//   firstName: "Afz",
+//   lastName: "Nom",
+//   describe: person1.describe,
+// };
+// console.log(person1Copy.describe());
--- src/app.ts
+++ src/app.ts
@@ -66,12 +66,86 @@ console.log(foo(5));
 
 //**this
 
-const arrowFun = () =>
-  console.log(
-    "'this' will be the surrounding of 'this', but as I am working in strinct environment so this will be undefined " +
-      this
-  );
+// const arrowFun = () =>
+//   console.log(
+//     "'this' will be the surrounding of 'this', but as I am working in strinct environment so this will be undefined " +
+//       this
+//   );
 
-arrowFun();
+// arrowFun();
 
 console.log(!!"a");
+
+/***************************** CLASSES ***********************/
+
+class Person {
+  // firstName: string;
+  // lastName: string;
+  private friends: string[] = [];
+
+  constructor(
+    private readonly id: number,
+    public firstName: string,
+    private lastName: string
+  ) {
+    // this.firstName = firstName;
+    // this.lastName = lastName;
+    // this.friends = [];
+
+    console.log("calling from object");
+  }
+
+  describe(this: Person): void {
+    console.log(`Person-${this.id}: ${this.firstName}  ${this.lastName}`);
+  }
+
+  addFriends(friend: string) {
+    this.friends.push(friend);
+  }
+
+  printFriendList() {
+    console.log(this.friends);
+  }
+}
+
+const person1 = new Person(1, "Afzal", "Nomani");
+
+person1.describe();
+
+person1.addFriends("Aditya");
+person1.addFriends("Imran");
+
+person1.printFriendList();
+
+//Inheritance
+
+class Student extends Person {
+  constructor(
+    id: number,
+    firstname: string,
+    lastName: string,
+    public cources: string[]
+  ) {
+    super(id, firstname, lastName);
+    this.cources = cources;
+  }
+}
+
+const stu1 = new Student(2, "karan", "Yadav", ["Node", "React"]);
+
+console.log(stu1);
+
+// console.log(person1.friends);
+
+// const person1Copy = { describe: person1.describe };
+// console.log(person1Copy.describe()); // for this 'this' will be undefined
+
+//quick fix
+
+// const person1Copy = {
+//   firstName: "Afz",
+//   lastName: "Nom",
+//   describe: person1.describe,
+// };
+
+// console.log(person1Copy.describe());
