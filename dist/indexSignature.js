2c3a8528bf1b3a90009814bfce2c60c1fdb5f89f
Afzal Nomani <afzalnomani1@gmail.com>
2023-03-12 21:01:40 +0530

    index signature

---
+++ dist/indexSignature.js
@@ -0,0 +1,15 @@
+"use strict";
+//index signature
+const airplane = {
+    flightNumber: "a207",
+    airplaneModel: "b76",
+    dateOfDeparture: "10/02/1999",
+    timeOfDeparture: "11:50",
+    from: "DLI",
+    to: "NKE",
+    seats: {
+        "1": "a207",
+        "2": "a208",
+        "3": "a209",
+    },
+};
---
+++ src/indexSignature.ts
@@ -0,0 +1,29 @@
+//index signature
+
+type Airplane = {
+  flightNumber: string;
+  airplaneModel: string;
+  dateOfDeparture: string;
+  timeOfDeparture: string;
+  from: string;
+  to: string;
+
+  seats: {
+    [k: string]: string; // this is called index signature, It can take multiple key value pair but must be as specified
+  };
+};
+
+const airplane: Airplane = {
+  flightNumber: "a207",
+  airplaneModel: "b76",
+  dateOfDeparture: "10/02/1999",
+  timeOfDeparture: "11:50",
+  from: "DLI",
+  to: "NKE",
+
+  seats: {
+    "1": "a207",
+    "2": "a208",
+    "3": "a209",
+  },
+};
