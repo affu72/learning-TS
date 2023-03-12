5020d910a59e1151117c3d8923996e9fa84fc227
Afzal Nomani <afzalnomani1@gmail.com>
2023-03-12 20:04:31 +0530

    union type

---
+++ dist/union.js
@@ -0,0 +1,33 @@
+"use strict";
+//Union Types
+const dog = {
+    name: "Buddy",
+    barks: true,
+    wags: true,
+};
+const cat = {
+    name: "Buddy",
+    purrs: false,
+    barks: false,
+};
+// const doggy: DogAndCatUnion = {
+//   name: "Ana",
+//   barks: false,
+// };
+const dogAndCat = {
+    name: "Hybrid",
+    wags: true,
+    purrs: false,
+    barks: false,
+};
+function addNumberOrString(a, b) {
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b;
+    }
+    else {
+        return a.toString() + b.toString(); // if any of the param is string hen convert another to string then add.
+    }
+}
+console.log(addNumberOrString(2, 3));
+console.log(addNumberOrString(2, "afzal"));
+// console.log(addNumberOrString(2, null));
---
+++ src/union.ts
@@ -0,0 +1,52 @@
+//Union Types
+
+type Dog = {
+  name: string;
+  barks: boolean;
+  wags: boolean;
+};
+
+type Cat = {
+  name: string;
+  purrs: boolean;
+};
+
+type DogAndCatUnion = Dog | Cat;
+
+const dog: DogAndCatUnion = {
+  name: "Buddy",
+  barks: true,
+  wags: true,
+};
+
+const cat: DogAndCatUnion = {
+  name: "Buddy",
+  purrs: false,
+  barks: false,
+};
+
+// const doggy: DogAndCatUnion = {
+//   name: "Ana",
+//   barks: false,
+// };
+
+const dogAndCat: DogAndCatUnion = {
+  name: "Hybrid",
+  wags: true,
+  purrs: false,
+  barks: false,
+};
+
+type numberOrString = number | string;
+
+function addNumberOrString(a: number | string, b: numberOrString) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  } else {
+    return a.toString() + b.toString(); // if any of the param is string hen convert another to string then add.
+  }
+}
+
+console.log(addNumberOrString(2, 3));
+console.log(addNumberOrString(2, "afzal"));
+// console.log(addNumberOrString(2, null));
